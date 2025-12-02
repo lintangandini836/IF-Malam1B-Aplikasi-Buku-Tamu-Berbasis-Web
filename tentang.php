9538cf229503827ba3516e45f6e5f7b4b2fd49bd
gasaragera-ux <gasaragera@gmail.com>
2025-12-02 20:17:42 +0700

    Tampilan tentang website buku tamu

---
+++ tentang.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tentang Website - PoliBatam</title>
+  <link rel="stylesheet" href="style1.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<link rel="icon" href="logo kampus.png" type="jpg/png">
+  </head>
+<body>
+
+  <!-- NAVBAR -->
+        <header>
+            <div class="navbar">
+                <div class="logo">
+                    <img src="logo kampus.png" alt="Polibatam">
+                    <Span>Polibatam</Span>
+                </div>
+                <nav>
+                    <a href="front-end.php">Beranda</a>
+                    <a href="tentang.php">Tentang</a>
+                    <a href="masukan_dan_saran.php">Masukan dan saran</a>
+                  </nav>
+                  <button type="button" class="button-primary">
+        Login Sebagai Admin
+      </button>
+            </div>
+        </header>
+
+  <!-- CONTENT -->
+  <main>
+    <section class="tentang">
+      <h2>Tentang Website ini</h2>
+      <p>
+        Website ini merupakan catatan riwayat kunjungan tamu yang datang ke kampus Politeknik Negeri Batam,
+        tujuan website ini dibuat agar pihak kampus atau admin Polibatam bisa melihat/mendata kunjungan dan tujuan tamu datang.
+        Website ini juga bisa digunakan ketika ada agenda-agenda kampus seperti seminar, kunjungan perusahaan,
+        yang bisa dihadiri oleh masyarakat umum maupun instansi lain.
+      </p>
+    </section>
+
+    <section class="developer">
+      <h2>Developer Website</h2>
+      <p>Website ini dibuat oleh kelompok 1 dari kelas IF-1B Malam, yang terdiri dari 3 anggota</p>
+
+      <div class="developer-list">
+        <div class="card">
+          <img src="alan.jpg" alt="Alan">
+          <p>Alan Farah Rohid Tanjung</p>
+        </div>
+        <div class="card">
+          <img src="lintang.jpg" alt="Lintang">
+          <p>Lintang Putri Andini</p>
+        </div>
+        <div class="card">
+          <img src="rafli.png" alt="Rafli">
+          <p>Muhammad Rafli Akbar Setiadi</p>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <!-- FOOTER -->
+        <footer class="footer">
+    <p>Tata Usaha Politeknik Negeri Batam</p>        
+    <p><i class="fa-solid fa-location-dot"></i> Jl. Ahmad Yani, Batam Centre - Kepulauan Riau | <i class="fa-solid fa-phone"></i> 107781 468855 Ext 102 | <i class="fa-solid fa-envelope"></i> TU@polibatam.ac.id</p>
+  </footer>    
+
+</body>
+</html>
